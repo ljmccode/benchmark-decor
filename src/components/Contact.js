b586c8af62c85ba5b2878a41b8ead191d0d00595
Laura <lmcsay@gmail.com>
2022-09-07 15:44:38 -0500

    Add info to content section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper className='section'>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            ut quis. Et rem amet esse adipisci sit aut, voluptatum corrupti.
+          </p>
+          <form action='' className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
-  padding: 5rem 0;
   h3 {
     text-transform: none;
   }
@@ -42,7 +63,7 @@ const Wrapper = styled.section`
     text-transform: capitalize;
   }
   .submit-btn {
-    background: var(--clr-primary-5);
+    background: var(--clr-primary-6);
     text-transform: capitalize;
     letter-spacing: var(--spacing);
     cursor: pointer;
@@ -67,6 +88,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
