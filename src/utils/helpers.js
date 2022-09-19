135c6e42f2908fa66508125e8bf50321af26c40c
Laura <lmcsay@gmail.com>
2022-09-19 13:42:13 -0500

    Get filter value from textContent if filter is a category

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,7 +7,6 @@ export const formatPrice = (number) => {
 
 export const getUniqueValues = (data, type) => {
   let unique = data.map((item) => item[type]);
-  console.log(type);
   if (type === 'colors') {
     unique = unique.flat();
   }
