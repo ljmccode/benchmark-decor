1fac3015b0f4bc74caea957f4939ca94fd5342e1
Laura <lmcsay@gmail.com>
2022-09-29 13:44:07 -0500

    Show filter buttons component when on a smaller screen

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Filters, ProductList, Sort, PageHero } from '../components';
+import FilterButtons from '../components/FilterButtons';
+import FilterModal from '../components/FilterModal';
 
 const ProductsPage = () => {
   return (
@@ -8,7 +10,13 @@ const ProductsPage = () => {
       <PageHero title='products' />
       <Wrapper className='page'>
         <div className='section-center products'>
-          <Filters />
+          <div className='filter-container'>
+            <Filters />
+          </div>
+          <div className='filter-buttons'>
+            <FilterButtons />
+          </div>
+          <FilterModal />
           <div>
             <Sort />
             <ProductList />
@@ -21,13 +29,30 @@ const ProductsPage = () => {
 
 const Wrapper = styled.div`
   .products {
-    display: grid;
-    gap: 3rem 1.5rem;
-    margin: 4rem auto;
+    margin: 2rem auto;
   }
+
+  @media (max-width: 767px) {
+    .filter-container {
+      display: none;
+    }
+    .filter-buttons {
+      display: grid;
+    }
+  }
+
   @media (min-width: 768px) {
     .products {
+      display: grid;
+      gap: 3rem 1.5rem;
+      margin: 4rem auto;
       grid-template-columns: 200px 1fr;
+    }
+    .filter-container {
+      display: flex;
+    }
+    .filter-buttons {
+      display: none;
     }
   }
 `;
